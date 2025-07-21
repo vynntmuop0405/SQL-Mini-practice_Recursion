887602d4b04e2751da0b0c314768a76ad55e7051
VyNguyen <nguyenngocthaovy451999@gmail.com>
2025-07-21 23:20:09 +0700

    update

--- Distribution.sql
+++ Distribution.sql
@@ -22,3 +22,20 @@ INSERT INTO distribution (ID, NAME, PARENT_ID, SALES) VALUES
 (12, 'ChiangmaiTeam', 5, 7000.00);
 
 -- Tính tổng doanh số của đại lý và các đại lý con
+
+    -- create hierachy with root_id represents for each branches
+WITH Distribution_hierachy AS (
+	SELECT ID, NAME, PARENT_ID, SALES, ID AS ROOT_ID
+	--, 0 AS LEVEL
+	FROM Distribution WHERE PARENT_ID IS NULL
+	UNION ALL
+	SELECT a.ID, a.NAME, a.PARENT_ID, a.SALES, b.ROOT_ID
+	FROM Distribution a
+	JOIN Distribution_hierachy b ON a.PARENT_ID = b.ID
+)
+    -- depend on what you want to calculate, if total sales of 1 branch than condition is the parent_id is null
+    -- if total accumulated sales of 1 branch then remove condition and name in group by
+SELECT NAME, ROOT_ID, SUM(SALES) Total_sale FROM Distribution_hierachy
+WHERE PARENT_ID IS NULL
+GROUP BY NAME, ROOT_ID
+	
