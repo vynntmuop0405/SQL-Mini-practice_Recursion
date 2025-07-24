527ebb2bcc5dfed28842fed4ffe9e8edf01c4789
VyNguyen <nguyenngocthaovy451999@gmail.com>
2025-07-25 00:59:00 +0700

    update

--- Expand.sql
+++ Expand.sql
@@ -1,4 +1,4 @@
---Cộng dồn Sales toàn bộ nhánh (bao gồm con cháu)
+    -- Cộng dồn Sales toàn bộ nhánh (bao gồm con cháu)
 
 With distribution_hierachy AS 
 ( SELECT id, name, parent_id, sales, id as root_id
@@ -14,6 +14,24 @@ With distribution_hierachy AS
 -- thì nó chỉ có 2 ID 1,2 thì sao bảng con có parent lên tới 3,4,5 JOIN được, em chưa tưởng tượng được
 SELECT a.root_id AS distribution_id, b.name Distribution_name,
 	   SUM(a.sales) Total_sales_include_children
+FROM distribution_hierachy a
+JOIN distribution b ON a.root_id = b.id
+GROUP BY a.root_id, b.name
+
+    -- Đếm số lượng con cháu mỗi Distributor (tương tự bài trên)
+    
+With distribution_hierachy AS 
+( SELECT id, name, parent_id, sales, id AS root_id
+  FROM distribution
+  WHERE parent_id IS NULL
+  UNION ALL
+  SELECT a.id, a.name, a.parent_id, a.sales, b.root_id
+  FROM distribution a
+  INNER JOIN distribution_hierachy b ON a.parent_id = b.id
+)
+--SELECT * FROM distribution_hierachy
+SELECT a.root_id, b.name Distribution_name, 
+	   COUNT(a.root_id)-1 AS count_children
 FROM distribution_hierachy a
 JOIN distribution b ON a.root_id = b.id
 GROUP BY a.root_id, b.name
