a21611f29d78f5a95876b587f477864ea05707e4
VyNguyen <nguyenngocthaovy451999@gmail.com>
2025-07-25 00:43:14 +0700

    update

--- Parts.sql
+++ Parts.sql
@@ -15,3 +15,16 @@ INSERT INTO Parts (Part_id, Part_name, Parent_part_id) VALUES
 (6, 'Part3', 3),
 (7, 'SubAssembly1', 3),
 (8, 'Part4', 7);
+
+--- Truy vết nguồn gốc sản phẩm
+WITH Part_hierachy AS (
+	SELECT part_id, part_name, parent_part_id, 0 LEVEL
+	FROM Parts
+	WHERE parent_part_id IS NULL
+	UNION ALL
+	SELECT a.part_id, a.part_name, a.parent_part_id, b.level+1
+	FROM Parts a
+	JOIN Part_hierachy b ON a.parent_part_id = b.part_id
+)
+SELECT * FROM Part_hierachy
+ORDER BY 1
